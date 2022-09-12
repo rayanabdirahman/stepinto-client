f55e3c688ad85d2a7c6e114761f402f369f7b087
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-09-13 00:28:24 +0100

    add AuthTemplate

---
+++ src/shared/components/templates/Auth/index.tsx
@@ -0,0 +1,56 @@
+import {
+  useColorModeValue,
+  HStack,
+  Box,
+  Center,
+  Stack,
+} from "@chakra-ui/react";
+import React from "react";
+import Container from "../Container";
+import { Heading, Logo, Text } from "../../UI/atoms";
+
+type Props = {
+  title: string;
+  subTitle: string;
+  children: React.ReactNode;
+};
+
+const AuthTemplate: React.FC<Props> = ({
+  title,
+  subTitle,
+  children,
+}: Props) => (
+  <Box as="section" h="100vh" bg="black" pt={{ base: 4, md: 4 }}>
+    <Container>
+      <HStack spacing="10" justify="space-between">
+        <Logo type="light" />
+      </HStack>
+
+      <Center>
+        <Box
+          w="md"
+          p={10}
+          bg="black"
+          rounded="md"
+          border="1px"
+          borderColor="gray.800"
+          boxShadow={useColorModeValue("sm", "sm-dark")}
+        >
+          <Stack spacing={6}>
+            <Stack>
+              <Heading styleType="light" as="h1" size="md" noOfLines={1}>
+                {title}
+              </Heading>
+              <Text styleType="gray" fontSize="md">
+                {subTitle}
+              </Text>
+            </Stack>
+            {children}
+          </Stack>
+        </Box>
+      </Center>
+    </Container>
+  </Box>
+);
+
+export default AuthTemplate;
