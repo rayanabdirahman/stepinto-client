047504139e8c9a3a17e7f321013f16143c3bdba6
rayanabdirahman <rayanmabdirahman@gmail.com>
2022-09-25 00:48:24 +0100

    remove auth user request from Home component

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,35 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Button } from "../shared/components/UI/atoms";
-import axiosHelper from "../shared/utilities/axiosHelper";
+import React from "react";
+import { Container } from "../shared/components/templates";
+import { Heading } from "../shared/components/UI/atoms";
+import withAuthentication from "../shared/HOC/withAuthentication";
 
-const Home: React.FC = () => {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    async function getUser() {
-      try {
-        const response: any = await axiosHelper.get("/accounts/auth/user");
-        const msg = `Hello ${response?.data?.user?.name}`;
-        setMessage(msg);
-      } catch (err) {
-        const msg = `Oops still not authenticated`;
-        setMessage(msg);
-      }
-    }
-
-    getUser();
-  }, []);
-
-  const signOut = async () => {
-    await axiosHelper.post("/accounts/auth/signout", {});
-  };
-
+const Home: React.FC = ({ user }: any) => {
   return (
-    <>
-      <Button onClick={signOut}>Sign out</Button>
-      <h1>{message || "You need to be signed in to view this page"}</h1>
-    </>
+    <Container>
+      <Heading>Hello World {user.name}</Heading>
+    </Container>
   );
 };
 
-export default Home;
+export default withAuthentication(Home);
